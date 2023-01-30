510d0399b74e71349910453a2149ead33a7cec7d
hyeonjin25 <shj41738866@gmail.com>
2023-01-30 16:17:09 +0900

    feat : add recoil-persist

--- src/state/UserData.js
+++ src/state/UserData.js
@@ -1,7 +1,11 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 // 신규 가입한 유저인지 여부
 export const newUserState = atom({
   key: "NewUser",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
